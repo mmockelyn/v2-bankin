3b75ce4b0ecbf3e2054901972ae7db3f3c2872ab
mmockelyn <trainznation@gmail.com>
2022-03-29 04:28:32 +0200

    290320220428
    - Ajout Telescope suivi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.debug') == true) {
             $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
         }
+        if ($this->app->environment('local')) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
